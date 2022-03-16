7a612a97bcb73607b4a9946273cd087d51c7b7ab
Daniel <daniel.villavicencio.mena@gmail.com>
2022-03-15 20:37:42 -0700

    Second commit

---
+++ src/main/java/com/example/chameleon/ChameleonApplication.java
@@ -0,0 +1,13 @@
+package com.example.chameleon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChameleonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChameleonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chameleon/ChameleonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chameleon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChameleonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
