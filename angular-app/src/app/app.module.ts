c8b0170389bfe9a87e1d891694e87ac833e28abc
danielvillavicencio <dany.villavicencio30@gmail.com>
2022-03-31 14:28:36 -0700

    Changed the splash component to be under the ../app directory

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { SignInComponent } from '../signin/signin.component';
-import { SplashPageComponent } from '../splash/splash.component';
+import { SignInComponent } from './signin/signin.component';
+import { SplashPageComponent } from './splash/splash.component';
 
 @NgModule({
   declarations: [
