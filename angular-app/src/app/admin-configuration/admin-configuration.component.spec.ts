3f321143a5c23370ae433f9b83ca3b0590d223ce
Daniel Ramos <rramsdany@gmail.com>
2022-04-02 21:57:00 -0700

    first commit

---
+++ angular-app/src/app/admin-configuration/admin-configuration.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminConfigurationComponent } from './admin-configuration.component';
+
+describe('AdminConfigurationComponent', () => {
+  let component: AdminConfigurationComponent;
+  let fixture: ComponentFixture<AdminConfigurationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminConfigurationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminConfigurationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
