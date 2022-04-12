6b093da71a74fed78256e6ce639fbc39d9aed463
danielvillavicencio <dany.villavicencio30@gmail.com>
2022-04-12 00:16:18 -0700

    #90 Create Google sign-in/registration

--- angular-app/src/app/splash/splash.component.ts
+++ angular-app/src/app/splash/splash.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-splash',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
 
-  constructor() { }
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer : DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      "background",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/subtle-prism.svg")
+    );
+  }
 
   ngOnInit(): void {
   }
