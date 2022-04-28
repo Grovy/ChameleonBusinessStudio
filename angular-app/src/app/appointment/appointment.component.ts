2485b445f4d68896d6abb85b3c2a7af7bd0cb000
Matt Crow <mattcrow19@gmail.com>
2022-04-28 14:03:38 -0700

    COM-149: list view Done with appointment list for senior project presentation. We'll revise this once we've settled on a consistent design for our site.

--- angular-app/src/app/appointment/appointment.component.ts
+++ angular-app/src/app/appointment/appointment.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { Appointment } from './model';
+
+/*
+we'll need to change this component a bit once we allow listing unavailable
+appointments.
+*/
 
 @Component({
     selector: 'appointment',
     templateUrl: './appointment.component.html',
-    styleUrls: []
+    styleUrls: ['./appointment.component.css']
 })
 export class AppointmentComponent {
+    @Input() appt?: Appointment;
     
+    constructor(){
+        
+    }
+    
+    public formatTime(time: string): string {
+        let dayStr = new Date(time).toLocaleString();
+        //console.log(dayStr);
+        return dayStr;
+    }
 }
