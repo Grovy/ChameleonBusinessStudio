6c4d5415d45be09b862635ed02a3c92035f7b116
Daniel Ramos <rramsdany@gmail.com>
2022-09-10 20:37:14 -0700

    COM 110 - Appointment Details View for Organizer

--- angular-app/src/app/admin-panel-test/admin-panel-test.component.ts
+++ angular-app/src/app/admin-panel-test/admin-panel-test.component.ts
@@ -1,9 +1,7 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from "@angular/material/tooltip";
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface UserData {
   name: string;
