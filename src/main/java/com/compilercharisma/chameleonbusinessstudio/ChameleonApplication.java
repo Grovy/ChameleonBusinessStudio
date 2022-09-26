d84a9ff8b404a326b34f9b22b3e3b7467abb00b6
Matt Crow <mattcrow19@gmail.com>
2022-09-26 14:20:57 -0700

    COM166 API endpoint for booking request.
    Added endpoint `/api/v1/appointments/book-me`, which books the given user for an appointment, if able. Also fixed many issues related to our transition to webflux.

--- src/main/java/com/compilercharisma/chameleonbusinessstudio/ChameleonApplication.java
+++ src/main/java/com/compilercharisma/chameleonbusinessstudio/ChameleonApplication.java
@@ -1,11 +1,21 @@
 package com.compilercharisma.chameleonbusinessstudio;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
 import com.compilercharisma.chameleonbusinessstudio.config.VendiaConfig;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
 import org.springframework.graphql.client.HttpGraphQlClient;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class ChameleonApplication {
@@ -24,4 +34,14 @@ public class ChameleonApplication {
                 .build();
     }
 
+    // serves index.html from "/" route
+    // https://stackoverflow.com/a/50324512
+    @Bean
+    public RouterFunction<ServerResponse> indexRouter(
+        @Value("classpath:/static/index.html") Resource indexHtml
+    ){
+        return route(GET("/"),
+            req->ok().contentType(MediaType.TEXT_HTML).bodyValue(indexHtml)
+        );
+    }
 }
