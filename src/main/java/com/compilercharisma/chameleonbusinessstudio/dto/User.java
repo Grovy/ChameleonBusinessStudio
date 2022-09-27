92d62a62bc0e60252ae7d27944e86b7172d4188f
danielvillavicencio <dany.villavicencio30@gmail.com>
2022-09-26 22:02:10 -0700

    Added javadoc to the entities

--- src/main/java/com/compilercharisma/chameleonbusinessstudio/dto/User.java
+++ src/main/java/com/compilercharisma/chameleonbusinessstudio/dto/User.java
@@ -17,14 +17,29 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class User implements Serializable {
 
+    /**
+     * Vendia Id of the user
+     */
     private String _id;
 
+    /**
+     * The display name of the user
+     */
     private String displayName;
 
+    /**
+     * The email of the user
+     */
     private String email;
 
+    /**
+     * The role of the user
+     */
     private UserRole role;
 
+    /**
+     * List of appointment ids that the user has
+     */
     private List<String> appointments;
 
 }
