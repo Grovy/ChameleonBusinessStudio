4fa3bb22b06fa5458884ce3359c65e95a43c2772
Daniel Ramos <rramsdany@gmail.com>
2022-10-03 17:27:00 -0700

    COM 211 Overhaul the Admin Panel Page with Better Styling

--- angular-app/src/app/models/interfaces/IUser.ts
+++ angular-app/src/app/models/interfaces/IUser.ts
@@ -3,19 +3,20 @@
     Email and role will be the only required fields, as we will default to email as display name.
 */
 
+import { IAppointment } from "./IAppointment";
+
 export enum UserRole {
-    'ADMIN',
-    'ORGANIZER',
-    'TALENT',
-    'PARTICIPANT',
+    'ADMIN' = 'Admin',
+    'ORGANIZER' = 'Organizer',
+    'TALENT' = 'Talent',
+    'PARTICIPANT' = 'Participant',
 }
 
 export interface IUser {
+    displayName: string;
     email: string;
     role: UserRole;
-    firstName?: string;
-    lastName?: string;
-    id?: string;
-    age?: number;
-    gender?: string;
+    id?: string; 
+    appointments?: IAppointment[];
+    
 }
