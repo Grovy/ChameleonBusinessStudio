6ffba37efef1f317c193f7233d6be339810d0ab6
Matt Crow <mattcrow19@gmail.com>
2022-10-10 19:03:01 -0700

    quick save

---
+++ src/main/java/com/compilercharisma/chameleonbusinessstudio/controller/ScheduleController.java
@@ -0,0 +1,10 @@
+package com.compilercharisma.chameleonbusinessstudio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/schedules")
+@RestController
+public class ScheduleController {
+    
+}
