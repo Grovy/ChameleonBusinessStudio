30d13b2ead78d1b36949734dfcdb86d1046ecd6d
Matt Crow <mattcrow19@gmail.com>
2022-10-21 12:30:14 -0700

    COM255 - BUG with UriComponentBuilder Fixed a bug where Spring Boot static helpers expect to run in a Servlet, not Webflux, by migrating to non-static UriComponentsBuilder

--- src/main/java/com/compilercharisma/chameleonbusinessstudio/controller/UserController.java
+++ src/main/java/com/compilercharisma/chameleonbusinessstudio/controller/UserController.java
@@ -1,17 +1,21 @@
 package com.compilercharisma.chameleonbusinessstudio.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.compilercharisma.chameleonbusinessstudio.dto.User;
 import com.compilercharisma.chameleonbusinessstudio.dto.UserResponse;
 import com.compilercharisma.chameleonbusinessstudio.service.UserService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
 
-import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(path="/api/v2/users")
