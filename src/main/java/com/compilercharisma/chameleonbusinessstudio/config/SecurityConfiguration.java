4b486e0aaf922f82a78bedfebffee046ee54c901
danielvillavicencio <dany.villavicencio30@gmail.com>
2022-10-27 17:46:15 -0700

    [Bugfix] Security Configuration for user registration flow

--- src/main/java/com/compilercharisma/chameleonbusinessstudio/config/SecurityConfiguration.java
+++ src/main/java/com/compilercharisma/chameleonbusinessstudio/config/SecurityConfiguration.java
@@ -20,6 +20,7 @@ public class SecurityConfiguration {
                 .oauth2Login()
                 .and()
                 .authorizeExchange()
+                .pathMatchers(HttpMethod.POST,   "/api/v1/users").authenticated()                // this will be used for the log in flow after a user is authenticated
                 .pathMatchers(HttpMethod.GET,    "/api/v1/**").authenticated()                   // by default, allow any logged-in user to GET
                 .pathMatchers(HttpMethod.POST,   "/api/v1/**").access(userAuthorizationManager)  // by default, only authorized users can POST
                 .pathMatchers(HttpMethod.PUT,    "/api/v1/**").access(userAuthorizationManager)  // by default, only authorized users can PUT
