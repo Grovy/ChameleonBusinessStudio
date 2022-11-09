0ebc107be8b991ff5ef64c70c87c3c88ba03f73c
Daniel <daniel.villavicencio.mena@gmail.com>
2022-11-08 22:13:40 -0800

    Removed the authentication package

--- src/main/java/com/compilercharisma/chameleonbusinessstudio/authentication/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package is responsible for validating the users who log into the 
- * application:
- * 1. are you who you say you are?
- * 2. are you registered with the website?
- */
-package com.compilercharisma.chameleonbusinessstudio.authentication;
