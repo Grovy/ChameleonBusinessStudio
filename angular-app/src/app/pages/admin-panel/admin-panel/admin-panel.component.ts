bd6f0cda373237576ee9a75f787efeb7d5a11a0b
Daniel Ramos <rramsdany@gmail.com>
2022-11-15 23:32:49 -0800

    Setting up admin panel

--- angular-app/src/app/pages/admin-panel/admin-panel/admin-panel.component.ts
+++ angular-app/src/app/pages/admin-panel/admin-panel/admin-panel.component.ts
@@ -3,6 +3,7 @@ import { IUser, UserRole } from "src/app/models/interfaces/IUser";
 import { IUserResponse } from "src/app/models/interfaces/IUserResponse";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { UserService } from "src/app/services/UserService.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component ({
   selector:'app-admin-panel',
@@ -14,16 +15,22 @@ export class AdminPanelComponent {
   
   myUserResponse: IUserResponse = { users: [] };
   displayedColumns: string[] = ['displayName', 'email', 'role'];
+  roleSelection: string[] = ['PARTICIPANT', 'ADMIN', 'TALENT', 'ORGANIZER'];
   selectedProfile: IUser | undefined;
   profileForm: FormGroup;
+  changeRoleForm: FormGroup;
+  reqCompleted = false;
 
-  constructor(private fb: FormBuilder, private userService: UserService) {
+  constructor(private fb: FormBuilder, private userService: UserService, private snackBar: MatSnackBar) {
     this.selectedProfile = this.myUserResponse.users ? this.myUserResponse.users[0] : undefined;
     this.profileForm = this.fb.group({
       displayName: [''],
       email: [''],
       confirmEmail: [''],
       role: [''],
+    });
+    this.changeRoleForm = this.fb.group({
+      newRole: ['', Validators.required]
     });
   }
 
@@ -32,6 +39,7 @@ export class AdminPanelComponent {
     this.userService.getAllUsers().subscribe(
       (data) => {
         this.myUserResponse = data;
+        this.reqCompleted = true;
       }
     );
   }
@@ -54,6 +62,35 @@ export class AdminPanelComponent {
     this.userService.createUser(newUser).subscribe(
       data => console.log(data)
     );
+  }
+
+  changeUserRole(data, user: IUser) {
+    let newUser: IUser = {
+      id: user.id,
+      displayName: user.displayName,
+      email: user.email,
+      role: data.newRole,
+      phoneNumber: user.phoneNumber,
+      appointments: user.appointments
+    };
+
+    this.userService.updateUser(newUser).subscribe(
+      data => {
+        if(data.status.toString() == '200'){
+          this.openSnackBar("Successfully updated user's role!", "Dismiss", {
+            duration: 5000,
+          });
+        } else {
+          this.openSnackBar("An error occured updating user's role", "Dismiss", {
+            duration: 5000,
+          });
+        }
+      }
+    );
+  }
+
+  openSnackBar(message, action?, config?) {
+    this.snackBar.open(message, action, config);
   }
 
   onDiscard() {
